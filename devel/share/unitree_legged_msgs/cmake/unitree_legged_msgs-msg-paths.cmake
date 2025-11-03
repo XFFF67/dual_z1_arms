341b2e0ce7254925041cfe7faeb571ed2c7872de
XFFF67 <115482053+XFFF67@users.noreply.github.com>
2025-11-03 17:32:34 +0800

    Delete devel directory

--- devel/share/unitree_legged_msgs/cmake/unitree_legged_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(unitree_legged_msgs_MSG_INCLUDE_DIRS "/home/xfff67/Unitree-Z1_SDK/z1_moveit_ws/src/z1_ros/unitree_ros/unitree_ros_to_real/unitree_legged_msgs/msg")
-set(unitree_legged_msgs_MSG_DEPENDENCIES std_msgs;geometry_msgs;sensor_msgs)
