6f512e4f2cfd8bb8a9f07beb225fc51a8a6dcb98
XFFF67 <115482053+XFFF67@users.noreply.github.com>
2025-11-23 11:57:54 +0800

    Delete build directory

--- build/z1_ros/unitree_ros/unitree_ros_to_real/unitree_legged_real/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xfff67/Unitree-Z1_SDK/z1_moveit_ws/src/z1_ros/unitree_ros/unitree_ros_to_real/unitree_legged_real
-# Build directory: /home/xfff67/Unitree-Z1_SDK/z1_moveit_ws/build/z1_ros/unitree_ros/unitree_ros_to_real/unitree_legged_real
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/z1_ros/z1_examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xfff67/Unitree-Z1_SDK/z1_moveit_ws/src/z1_ros/z1_examples
-# Build directory: /home/xfff67/Unitree-Z1_SDK/z1_moveit_ws/build/z1_ros/z1_examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
